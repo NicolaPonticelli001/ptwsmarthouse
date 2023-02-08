e1fe2420cb6cf70c64f9c5d38db2b04aa71ea1da
Nicola <nicola.ponticelli831@gmail.com>
2023-02-08 18:17:57 +0100

    Wifi client almost complete
    The wifi page has been almost completed. Now there is the functionality to add manually the ssid and password of a wifi network (it still need js validation).
    The network scan code for esp32 is completed. Now is needed the functionality that allows esp to connect to wifi network that has been sent by the client and save it inside flash memori (config.txt file using SPIFFS)

--- esp32/data/webserver/script.js
+++ esp32/data/webserver/script.js
@@ -1,19 +1,20 @@
 let requestDelay = 5000
 let networkIntervalObj;
 
-function extractIdNetwork(fullId) {
-  const splitArray = fullId.split("-")
-  return splitArray[splitArray.length - 1]
+window.addEventListener("load", onBodyLoad);
+
+function showManualConnection() {
+  let container = document.getElementById("credentials-fixed-container").classList.toggle("hide")
+  if (container.classList.contains("hide")) networkIntervalObj = setInterval(getNetworks, requestDelay)
+  else clearInterval(networkIntervalObj)
 }
 
-function deleteInput(buttonId) {
-  let networkId = extractIdNetwork(buttonId)
-  document.getElementById("password-" + networkId).value = ""
+function deleteInput(buttonElement) {
+  buttonElement.previousElementSibling.value = ""
 }
 
-function showPasswordBox(infoBoxId) {
-  let networkId = extractIdNetwork(infoBoxId)
-  let passwordBoxElement = document.getElementById("passwordBox-" + networkId);
+function showPasswordBox(networkNumber) {
+  let passwordBoxElement = document.getElementById("passwordBox-" + networkNumber);
 
   document.querySelectorAll(".passwordBox").forEach( element => {
     if (element.id != passwordBoxElement.id) element.classList.add("hide")
@@ -24,7 +25,11 @@ function showPasswordBox(infoBoxId) {
 }
 
 function onBodyLoad() {
-
+  document.getElementById("manualConnectionBtn").addEventListener("click", showManualConnection)
+  let deleteButtons = document.getElementsByClassName("delete-btn")
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function() { deleteInput(deleteButtons[i]) })
+  }
   getNetworks()
   networkIntervalObj = setInterval(getNetworks, requestDelay)
 }
@@ -32,6 +37,7 @@ function onBodyLoad() {
 function getNetworks() {
   const xhttp = new XMLHttpRequest()
   xhttp.onload = function() {
+    //let test = '{"total":2,"0":{"rssi":-65,"ssid":"sassari","auth":3},"1":{"rssi":-34,"ssid":"palermo","auth":0}}'
     let networkListContainer = document.querySelector("#network-list-container")
     networkListContainer.textContent = ""
 
@@ -45,18 +51,27 @@ function getNetworks() {
       for (let i = 0, total = parseInt(jsonObj["total"]); i < total; i++) {
         let singleNetworkContainer = networkTemplate.content.cloneNode(true);
 
+        //Get the elements that need the id to be completed with the network number
         let toCompleteId = singleNetworkContainer.querySelectorAll("[data-completeId]")
         for (let j = 0; j< toCompleteId.length; j++) {
+          if (toCompleteId[j].id == "delete") toCompleteId[j].addEventListener("click", function() { deleteInput(toCompleteId[j]) })
+          if (toCompleteId[j].id == "infoBox") toCompleteId[j].addEventListener("click", function() { showPasswordBox(i) })
           toCompleteId[j].id += "-" + i
           toCompleteId[j].removeAttribute("data-completeId")
         }
 
+        //Get the elements that are gonna contains the json data
         let toParseData = singleNetworkContainer.querySelectorAll("[data-parseData]")
         for (let j = 0; j < toParseData.length; j++) {
           toParseData[j].textContent = jsonObj[i.toString()][toParseData[j].id.toLowerCase()]
           toParseData[j].id += "-" + i
           toParseData[j].removeAttribute("data-parseData")
         }
+
+        //Set the value of hidden input ssid
+        let hiddenInputSsid = singleNetworkContainer.querySelector('input[name="ssid"]')
+        console.log(hiddenInputSsid)
+        hiddenInputSsid.setAttribute("value", jsonObj[i.toString()]["ssid"])
 
         networkListContainer.appendChild(singleNetworkContainer)
         if (i != total-1) networkListContainer.appendChild(separatorTemplate.content.cloneNode(true))
