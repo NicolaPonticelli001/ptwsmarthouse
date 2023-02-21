b06eb0465d40c304d69e396dbf356ce16823aae8
Francesco-Peluso <75223409+Francesco-Peluso@users.noreply.github.com>
2023-02-21 19:25:38 +0100

    MQTT message sending
    Added the MQTT message sending to the server and the respective status update

--- website/SmartHouse.py
+++ website/SmartHouse.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+import requests
 import os
 import psycopg2
 import json
@@ -10,48 +11,20 @@ import time
 import paho.mqtt.client as paho
 from paho import mqtt
 
-# setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
 
-# with this callback you can see if your publish was successful
 def on_publish(client, userdata, mid, properties=None):
     print("mid: " + str(mid))
 
-# print which topic was subscribed to
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-# print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
     print("|Topic: " + msg.topic + "| - |Qos: " + str(msg.qos) + "| - |Payload: " + str(msg.payload) + "|")
+    result=requests.post(f'http://192.168.1.41:9600/device/update_status',data="{} {}".format(msg.topic,msg.payload.decode('utf-8')))
+    result.close()
 
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
-client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
-client.on_connect = on_connect
-
-# enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client.username_pw_set("raspserver", "Raspserver-2023")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("6dd678185e194e4ca3c7ffd5fe9abf15.s2.eu.hivemq.cloud", 8883)
-
-# setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
-client.on_message = on_message
-client.on_publish = on_publish
-
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("#", qos=2)
-
-# a single publish, this can also be done in loops, etc.
-#client.publish("test", payload="test send", qos=1)
-
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
 
 app=Flask(__name__)
 app.config["SESSION_PERMANENT"]=False
@@ -102,7 +75,7 @@ def return_house_json():
 		y=json.dumps(x)
 		return y
 	else:
-		return {"error_type":"SessionMissing","description":"No Session existing for this user"}
+		return {"status":"session missing"}
 
 @app.route("/home/account")
 def account():
@@ -180,16 +153,21 @@ def loginverification():
 		email=request.form['email']
 		password=request.form['password']
 		cur.execute('SELECT email,password FROM Customer WHERE email= %s',[email])
-		rows=cur.fetchone()
-		conn.commit()
+		if cur.rowcount!=0:
+			rows=cur.fetchone()
+			conn.commit()
 	
-		cur.close()
-		conn.close()
+			cur.close()
+			conn.close()
 		
-		bcrypt=Bcrypt(app)
-		if bcrypt.check_password_hash(rows[1],password):
-			session['user']=request.form['email']
-			return redirect(url_for('home'))
+			bcrypt=Bcrypt(app)
+			if bcrypt.check_password_hash(rows[1],password):
+				session['user']=request.form['email']
+				return redirect(url_for('home'))
+			else:
+				return redirect(url_for('login'))
+		else:
+			return redirect(url_for('login'))
 	else:
 		return redirect(url_for('login'))
 
@@ -225,7 +203,7 @@ def register_user():
 		conn.commit()
 		num_row=cur.fetchone()
 		if(num_row[0]==1):
-			return "Accont già esistente per questa e-mail"
+			return redirect(url_for('register'))
 		else:
 			cur.execute('INSERT INTO Customer (email,password,telephone,name,surname) VALUES (%s,%s,%s,%s,%s)',[email,hashed_password,telephone,name,surname])
 			conn.commit()
@@ -258,8 +236,6 @@ def login_device():
 		
 		bcrypt=Bcrypt(app)
 		if bcrypt.check_password_hash(rows[1],password):
-			session['device_user']=request.form['email']
-			
 			cur_house=conn.cursor()
 			cur_room=conn.cursor()
 		
@@ -288,28 +264,79 @@ def login_device():
 def add_device():
 	#Insert iotdevice
 	#"house","room","name","type","onoff=f"
-	if session.get("device_user"):
-		if reques.method=="POST":
-			conn=psycopg2.connect(
-			host="localhost",
-			database="ptw_smart_house_db",
-			user="ptw_admin",
-			password="SUPER_ROOT")
-	
-			cur=conn.cursor()
-			query="INSERT INTO IoTDevice (deviceCode,idHouse,idRoom,name,type,onoff) VALUES (DEFAULT,%s,%s,%s,%s,False)"
-			cur.execute(query,[request.form["house"],request.form["room"],request.form["name"],request.form["type"]])
-			if cursor.rowcount==1:
-				return json.dumps({"status":"inserted"})
-			else:
-				return json.dumps({"status":"insert fail"})
+	if reques.method=="POST":
+		conn=psycopg2.connect(
+		host="localhost",
+		database="ptw_smart_house_db",
+		user="ptw_admin",
+		password="SUPER_ROOT")
+		cur=conn.cursor()
+		query="INSERT INTO IoTDevice (deviceCode,idHouse,idRoom,name,type,onoff) VALUES (DEFAULT,%s,%s,%s,%s,False)"
+		cur.execute(query,[request.form["house"],request.form["room"],request.form["name"],request.form["type"]])
+		if cur.rowcount==1:
+			return json.dumps({"status":"inserted"})
 		else:
-			return json.dumps({"status":"method not allowd"})
+			return json.dumps({"status":"insert fail"})
 	else:
-		return json.dumps({"status":"session missing"})
+		return json.dumps({"status":"method not allowd"})
+
+@app.route("/device/update_status",methods=["POST"])
+def update_status():
+	if request.method=="POST":
+		topic=request.data.decode('utf-8')
+		print(topic)
+		ii=0
+		for i in range(len(topic)):
+			if topic[i]=="/":
+				ii=i
+				break
+		house=topic[0:ii]
+		topic=topic[ii+1:len(topic)]
+		for i in range(len(topic)):
+			if topic[i]=="/":
+				ii=i
+				break
+		room=topic[0:ii]
+		topic=topic[ii+1:len(topic)]
+		for i in range(len(topic)):
+			if topic[i]==" ":
+				ii=i
+				break
+		device=topic[0:ii]
+		mqtt_message=topic[ii+1:len(topic)]
+		if mqtt_message=="on":
+			mqtt_message=True
+		else:
+			mqtt_message=False
+		
+		conn=psycopg2.connect(
+		host="localhost",
+		database="ptw_smart_house_db",
+		user="ptw_admin",
+		password="SUPER_ROOT")
+		cur=conn.cursor()
+		query="UPDATE IoTDevice SET OnOff=%s WHERE devicecode=%s AND idhouse=%s AND idroom=%s" 
+		cur.execute(query,[mqtt_message,device,house,room])
+		conn.commit()
+		conn.close()
+		conn.close()
+		if cur.rowcount==1:
+			return {"status":"updated"}
+		else:
+			return {"status":"error"}
+			
+	return {"status":"error"}
 
 if __name__=="__main__":
+	client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+	client.on_connect = on_connect
+	client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+	client.username_pw_set("raspserver", "Raspserver-2023")
+	client.connect("6dd678185e194e4ca3c7ffd5fe9abf15.s2.eu.hivemq.cloud", 8883)
+	client.on_subscribe = on_subscribe
+	client.on_message = on_message
+	client.on_publish = on_publish
+	client.subscribe("#", qos=2)
 	client.loop_start()
 	app.run(host='0.0.0.0',port=9600,debug=True)
 	
-
